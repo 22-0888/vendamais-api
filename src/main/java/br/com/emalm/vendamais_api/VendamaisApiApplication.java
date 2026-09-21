009e739f9d6d338740d243f644fd914cb0d5424e
Eliane <mendes.eliane08@gmail.com>
2026-09-21 13:28:32 -0300

    primeiro commit vendamais-api

---
+++ src/main/java/br/com/emalm/vendamais_api/VendamaisApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.emalm.vendamais_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendamaisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendamaisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/emalm/vendamais_api/VendamaisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emalm.vendamais_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendamaisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
